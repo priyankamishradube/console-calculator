8f70dc50b1328a556df925ef6dc238708092821a
priyankamishradube <78054342+priyankamishradube@users.noreply.github.com>
2021-02-11 00:32:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from replit import clear
+from art import logo
 def add(n1,n2):
   return n1+n2
 
@@ -21,25 +22,29 @@ operations ={
   "*":multiply,
   "/":divide
 }
-should_continue = True
-num1 = int(input("Enter the first number "))
-for i in operations:
-  print(i)
+def calculator():
+  print(logo)
+  should_continue = True
+  num1 = float(input("Enter the first number "))
+  for i in operations:
+    print(i)
 
-while should_continue:
-  user_operator = input("Pick an operation ")
-  num2 = int(input("Enter the next number "))
+  while should_continue:
+    user_operator = input("Pick an operation ")
+    num2 = float(input("Enter the next number "))
 
-  calculation_function = operations[user_operator]
-  answer = calculation_function(num1,num2)
-  print(f"{num1} {user_operator} {num2} = {answer}")
+    calculation_function = operations[user_operator]
+    answer = calculation_function(num1,num2)
+    print(f"The answer is : {answer}")
 
-  response = input("Would you like to continue? Y or N").lower()
-  if response == "y":
-    num1= answer
-  else:
-    should_continue = False
-    clear()
+    response = input("Would you like to continue? Y or N").lower()
+    if response == "y":
+      num1= answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
 
 
 
